67202e0a7a846891ec74f7e8ac9df5c1ef9dbf10
wntitus <wesn.titus@gmail.com>
2018-07-27 21:17:23 -0400

    finished logic for adding more buttons and adding comments

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -8,6 +8,7 @@ $( document ).ready( function() {
     // declaring function to populate page with buttons for each topic
 
     function btnPopulate() {
+        $(".topicArray").empty();
         for (i = 0; i < topics.length; i++) {
             var topicBtn = $("<button>");
             $(topicBtn).attr("type", "button");
@@ -18,10 +19,27 @@ $( document ).ready( function() {
         }
     }
 
+    // populating page with initial buttons
+
     btnPopulate();
 
+    // click function for the "add choice" button so user inputs can be added to the available buttons, if the user inputs nothing
+    // the button will not be added
 
-    $(".gifBtn").on("click", function() {
+    $(".addChoice").on("click", function() {
+        var userChoice = $(".userText").val().trim();
+        if (userChoice === "") {
+            return;
+        }
+        topics.push(userChoice);
+        btnPopulate();
+    })
+
+
+    // click function for the main buttons, uses the ajax call for 10 gifs based on which button the user clicked and adds them to the page
+    // in still form, as well as displaying the rating of the gif below them
+
+    $(document).on("click", ".gifBtn", function() {
         var topic = $(this).val();
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=AcN1tGKKi7bbptq3UtIL3QTwWEFl0EPX&limit=10"
         $(".gifBox").empty();
@@ -34,26 +52,37 @@ $( document ).ready( function() {
                 var respIndex = response.data[i];
                 var stillGif = respIndex.images.fixed_height_still.url;
                 var activeGif = respIndex.images.fixed_height.url;
+                var rating = respIndex.rating;
+                var ratingBox = $("<h3>");
+                var imgBox = $("<div>");
+                $(imgBox).attr("class", "imgBox");
+                $(ratingBox).append(rating);
                 var imgElem = $("<img>");
                 $(imgElem).attr("data-still", stillGif);
                 $(imgElem).attr("data-active", activeGif);
                 $(imgElem).attr("data-state", "still");
                 $(imgElem).attr("class", "animalGif");
                 $(imgElem).attr("src", stillGif);
-                $(".gifBox").append(imgElem);
+                $(".gifBox").append(imgBox);
+                $(imgBox).append(imgElem);
+                $(imgBox).append(ratingBox);
             }
-            $("img").on("click", function() {
-                var gifState = $(this).attr("data-state");
-                if (gifState === "still") {
-                    $(this).attr("src", $(this).attr("data-active"));
-                    $(this).attr("data-state", "active");
-                } else if (gifState === "active"){
-                    $(this).attr("src", $(this).attr("data-still"));
-                    $(this).attr("data-state", "still");
-                }
-            })
+
         })
     
+    })
+
+    // stop and start click function, when user clicks a still image the src attribute is changed to the active gif and vice versa
+
+    $(document).on("click", ".animalGif", function() {
+        var gifState = $(this).attr("data-state");
+        if (gifState === "still") {
+            $(this).attr("src", $(this).attr("data-active"));
+            $(this).attr("data-state", "active");
+        } else if (gifState === "active"){
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
     })
 
    
